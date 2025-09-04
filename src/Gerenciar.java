d80a3818a8e99ca7be34b355e7ebae63b8114307
mathe <matheuseqc@gmail.com>
2025-09-04 15:49:57 -0300

    feat: mudanca no nome da classe Main para Gerenciar

--- src/Gerenciar.java
+++ src/Gerenciar.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Gerenciar {
     public static void main(String[] args) {
         //Questao 1
         Caneta canetaAzul = new Caneta("Caneta Azul", 50);
